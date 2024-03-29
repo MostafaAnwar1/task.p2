72c87f05b199664d50f3383cf5cfea60f23ffb30
MostafaAnwar1 <sasaanwar521@gmail.com>
2024-03-29 15:20:45 +0200

    solve python function task

---
+++ task.p/task.py.shell.py
@@ -0,0 +1,23 @@
+Python 3.11.8 (tags/v3.11.8:db85d51, Feb  6 2024, 21:52:07) [MSC v.1937 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======== RESTART: C:/Users/pc123/OneDrive/Desktop/task.py2/task.py.2.py ========
+>>> 
+======== RESTART: C:/Users/pc123/OneDrive/Desktop/task.py2/task.py.2.py ========
+35
+35
+>>> 
+======== RESTART: C:/Users/pc123/OneDrive/Desktop/task.py2/task.py.2.py ========
+20
+>>> 
+======== RESTART: C:/Users/pc123/OneDrive/Desktop/task.py2/task.py.2.py ========
+0 0
+>>> 
+======== RESTART: C:/Users/pc123/OneDrive/Desktop/task.py2/task.py.2.py ========
+sum of the arguments: 21
+>>> 
+======== RESTART: C:/Users/pc123/OneDrive/Desktop/task.py2/task.py.2.py ========
+Sum of the arguments: 21
+>>> 
+======== RESTART: C:/Users/pc123/OneDrive/Desktop/task.py2/task.py.2.py ========
+Sum of the arguments: 21
